fb25eb79757344d38d179d4a7cbc22ce42c472f0
devsince2021 <dev.ryu2021@gmail.com>
2024-01-23 02:56:22 +0900

    chore: 생성 api 명세 간소화

--- src/main/java/com/jdh/community_spring/domain/post/controller/PostController.java
+++ src/main/java/com/jdh/community_spring/domain/post/controller/PostController.java
@@ -1,15 +1,10 @@
 package com.jdh.community_spring.domain.post.controller;
 
-import com.jdh.community_spring.common.dto.HttpErrorInfo;
 import com.jdh.community_spring.domain.post.domain.Post;
 import com.jdh.community_spring.common.dto.ListReqDto;
 import com.jdh.community_spring.domain.post.dto.CreateReqDto;
 import com.jdh.community_spring.domain.post.service.interfaces.PostService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -27,13 +22,7 @@ public class PostController {
 
   private final PostService postService;
 
-
   @Operation(summary = "게시글 생성", description = "제목, 내용, 작성자, 카테고리를 포함하는 게시글을 작성합니다.")
-  @ApiResponses(value = {
-          @ApiResponse(responseCode = "201", description = "게시물 생성 성공하였습니다."),
-          @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 입력 데이터를 확인하세요."),
-          @ApiResponse(responseCode = "500", description = "서버 내부 오류가 발생했습니다.")
-  })
   @ResponseStatus(HttpStatus.CREATED)
   @PostMapping("/post")
   public void createPost(@Valid @RequestBody CreateReqDto dto) {
