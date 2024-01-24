00d1924c7d3930097e32f90f2d663d66d94c8d5f
devsince2021 <dev.ryu2021@gmail.com>
2024-01-24 18:54:09 +0900

    add: 누락된 스웨거 내용 추가

--- src/main/java/com/jdh/community_spring/common/dto/ListReqDto.java
+++ src/main/java/com/jdh/community_spring/common/dto/ListReqDto.java
@@ -1,5 +1,6 @@
 package com.jdh.community_spring.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,14 +21,19 @@ import javax.validation.constraints.Min;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ListReqDto {
+
+  @Schema(description = "페이지 (1부터 시작)", example = "1")
   @Min(value = 1, message = "page는 최소 1 이상이어야 합니다.")
   private int page = 1;
 
+  @Schema(description = "페이지 사이즈", example = "10")
   @Min(value = 1, message = "size 최소 1 이상이어야 합니다.")
   private int size = 10;
 
+  @Schema(description = "정렬 기준", example = "createdAt")
   private String sortBy = "createdAt";
 
+  @Schema(description = "오름차순/내림차순", example = "asc/desc")
   private String orderBy = "desc";
 
   // NOTE: 프론트에서는 page가 1부터 시작하고 있어서 서버에서 -1을 합니다.
