bb2dedcb24216bceb6f4f812683032e01a241e3b
devsince2021 <dev.ryu2021@gmail.com>
2024-02-02 16:58:51 +0900

    Revert "add: 개발 서버 동작 유지를 위한 테스트 컨트롤러 작성"
    This reverts commit 7a5f7a3c3ce6aea0b576fafe63512589930b81b4.

--- src/main/java/com/jdh/community_spring/domain/post/service/PostServiceImpl.java
+++ src/main/java/com/jdh/community_spring/domain/post/service/PostServiceImpl.java
@@ -66,38 +66,9 @@ public class PostServiceImpl implements PostService {
   }
 
 
-
+  @Transactional
   @Override
   public PostResDto getPost(long postId) {
-    Optional<Post> optPost = postRepository.findById(postId);
-    Post post = optPost.orElseThrow(() -> new NotFoundException("[postId: " + postId + "] 게시글이 존재하지 않습니다"));
-    post.setViewCount(post.getViewCount() + 1);
-    postRepository.save(post);
-
-    PostResDto postResDto = new PostResDto();
-    postResDto.setPostId(post.getPostId());
-    postResDto.setTitle(post.getTitle());
-    postResDto.setContent(post.getTextContent());
-    postResDto.setCategory(post.getCategory());
-    postResDto.setViewCount(post.getViewCount());
-    postResDto.setCreator(post.getCreator());
-    postResDto.setCreatedAt(post.getCreatedAt());
-
-    List<Comment> list = post.getComments();
-    List<CommentResDto> comments = list.stream()
-            .map((c) -> commentMapper.toCommentResDto(c))
-            .collect(Collectors.toList());
-
-    postResDto.setComments(comments);
-
-    return postResDto;
-  }
-
-
-
-  // MAP: getPost
-  @Transactional
-  public PostResDto getPost_Test(long postId) {
     Optional<Post> optPost = postRepository.findByIdInPessimisticWrite(postId);
     Post post = optPost.orElseThrow(() -> new NotFoundException("[postId: " + postId + "] 게시글이 존재하지 않습니다"));
     post.setViewCount(post.getViewCount() + 1);
