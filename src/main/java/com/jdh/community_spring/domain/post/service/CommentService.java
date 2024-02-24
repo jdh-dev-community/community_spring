d8c435425be0cd90ea51abd785b04a4bc725f317
devsince2021 <dev.ryu2021@gmail.com>
2024-02-24 16:19:48 +0900

    refac: 미사용 dto 제거

--- src/main/java/com/jdh/community_spring/domain/post/service/CommentService.java
+++ src/main/java/com/jdh/community_spring/domain/post/service/CommentService.java
@@ -3,7 +3,6 @@ package com.jdh.community_spring.domain.post.service;
 import com.jdh.community_spring.common.dto.ListReqDto;
 import com.jdh.community_spring.domain.post.dto.CommentDto;
 import com.jdh.community_spring.domain.post.dto.CommentCreateReqDto;
-import com.jdh.community_spring.domain.post.dto.CommentResDto;
 
 import java.util.List;
 
