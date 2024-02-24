3745ce193b5bfc78c6a865e59c7a10b76d634e9b
devsince2021 <dev.ryu2021@gmail.com>
2024-02-24 14:01:04 +0900

    feat: 엔티티 업데이트 로직 patch에서 put으로 변경

--- src/main/java/com/jdh/community_spring/domain/post/domain/Post.java
+++ src/main/java/com/jdh/community_spring/domain/post/domain/Post.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 
 @Builder
@@ -28,7 +30,7 @@ public class Post extends BaseEntity {
   private String title;
 
   @Schema(description = "게시글의 내용", example = "When I start my server, the error below shows")
-  @Column(name = "text_content", nullable = false, columnDefinition = "TEXT" )
+  @Column(name = "text_content", nullable = false, columnDefinition = "TEXT")
   private String textContent;
 
 
@@ -63,9 +65,15 @@ public class Post extends BaseEntity {
   }
 
   public void update(String title, String textContent, String creator, String category) {
-    if (title != null) this.title = title;
-    if (textContent != null) this.textContent = textContent;
-    if (creator != null) this.creator = creator;
-    if (category != null) this.category = category;
+    List<String> inputs = List.of(title, textContent, creator, category);
+    inputs.stream().filter(Objects::isNull).findFirst().ifPresent((n) -> {
+      throw new IllegalArgumentException("잘못된 수정 요청입니다. 입력을 확인해주세요");
+    });
+
+
+    this.title = title;
+    this.textContent = textContent;
+    this.creator = creator;
+    this.category = category;
   }
 }
